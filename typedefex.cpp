857732155cafaac16e2f68a62e036def0e0a9ceb
Vikas <56231875+VikasShreshthi@users.noreply.github.com>
2021-08-07 14:28:29 +0530

    Create typedefex.cpp

---
+++ typedefex.cpp
@@ -0,0 +1,3 @@
+typedef int myint; // myint can be used insted of int
+typedef int*  myintptr; // myintptr can be used insted of int*
+typedef int (*add) (int,int); // add can be used for callback declaration add p1 which is same as int (*p1) (int, int)
